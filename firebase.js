a5e7e0004c3e0e85a7d305f95430573c4d73e31e
정제우 <wpdn3825@naver.com>
2023-04-21 01:58:03 +0900

    firebase 설치

---
+++ firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBcMJ_uCwtqEjxe9gz_rba120HHqc5Fgfc",
+  authDomain: "nwitter-92520.firebaseapp.com",
+  projectId: "nwitter-92520",
+  storageBucket: "nwitter-92520.appspot.com",
+  messagingSenderId: "341583554826",
+  appId: "1:341583554826:web:3df02a1aedd133a858f2f3",
+};
+
+export default firebase.initializeApp(firebaseConfig);
